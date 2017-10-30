28d7d6d05f8bd61eb28584c9b5cd033a5534c163
stevenvill <stevenvillarreal8@gmail.com>
2017-10-29 23:31:08 -0500

    integrated working fb login button

--- Group14Alpha/Group14Alpha/ViewController.swift
+++ Group14Alpha/Group14Alpha/ViewController.swift
@@ -7,23 +7,45 @@
 //
 
 import UIKit
-import FacebookLogin
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = LoginButton(readPermissions: [ .PublicProfile ])
-        loginButton.center = view.center
         
+        let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
+        loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
+        loginButton.readPermissions = ["email", "public_profile"]
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
 
+        FBSDKGraphRequest(graphPath: "/me", parameters: ["fields": "id, name, email"]).start { (connection, result, err) in
+            
+            if err != nil {
+                print("Failed to start graph request:", err!)
+                return
+            }
+            print(result!)
+        }
+    }
 
 }
 
---
+++ Group14Alpha/Pods/Target Support Files/Pods-Group14Alpha/Pods-Group14Alpha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Group14Alpha : NSObject
+@end
+@implementation PodsDummy_Pods_Group14Alpha
+@end
